50d4fc2e475ba301c809612f973f64ea650b40e4
Hajun Song <hjsong@barogo.com>
2023-07-09 03:24:17 +0900

    checkAdjacentColors jsDocs 생성 및 대칭배열 구하는 함수 생성

--- src/lib/mapUtility.js
+++ src/lib/mapUtility.js
@@ -1,9 +1,25 @@
-const checkAdjacentColors = (map, row, col) => {
+export const transposeMap = (map) => {
+  const transposedMap = [];
+
+  map[0].forEach((_, col) => {
+    const transposedRow = map.map((row) => row[col]);
+    transposedMap.push(transposedRow);
+  });
+
+  return transposedMap;
+};
+
+/** 인접한 색을 확인합니다.
+ * 주어진 방향의 인접한 Cell의 색상이 다르다면 true를 반환합니다.
+ * 또, 주어진 방향에 인접한 셀이 없다면(배열의 끝이라면) true를 반환합니다.
+ * true는 추후 MapCell에서 검은색 경계선으로 치환됩니다.
+ */ 
+export const checkAdjacentColors = (map, row, col) => {
   const directions = {
-    top: { row: -1, col: 0 },
-    bottom: { row: 1, col: 0 },
-    left: { row: 0, col: -1 },
-    right: { row: 0, col: 1 },
+    left: { row: -1, col: 0 },
+    right: { row: 1, col: 0 },
+    top: { row: 0, col: -1 },
+    bottom: { row: 0, col: 1 },
   };
 
   const color = map[row][col];
@@ -23,11 +39,9 @@ const checkAdjacentColors = (map, row, col) => {
       const adjacentColor = map[adjacentRow][adjacentCol];
       adjacentColors[direction] = adjacentColor !== color;
     } else {
-      adjacentColors[direction] = false;
+      adjacentColors[direction] = true;
     }
   }
 
   return adjacentColors;
 };
-
-export default checkAdjacentColors;
