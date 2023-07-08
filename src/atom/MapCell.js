d792b3a45efd9f6b93d0211263e7fe6a8afed8b9
Hajun Song <hjsong@barogo.com>
2023-07-09 03:23:26 +0900

    MapCell border 두께 조절

--- src/atom/MapCell.js
+++ src/atom/MapCell.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const MapCell = ({ color, size, hasDifferentAdjacent }) => {
-  console.log(hasDifferentAdjacent);
   return (
     <div
       className="map-cell"
@@ -9,10 +8,10 @@ const MapCell = ({ color, size, hasDifferentAdjacent }) => {
         backgroundColor: color,
         width: `${size}px`,
         height: `${size}px`,
-        borderTop: hasDifferentAdjacent.top ? '2px solid black' : `none`,
-        borderBottom: hasDifferentAdjacent.bottom ? '2px solid black' : `none`,
-        borderLeft: hasDifferentAdjacent.left ? '2px solid black' : `none`,
-        borderRight: hasDifferentAdjacent.right ? '2px solid black' : `none`,
+        borderTop: hasDifferentAdjacent.top ? '1.5px solid black' : `none`,
+        borderBottom: hasDifferentAdjacent.bottom ? '1.5px solid black' : `none`,
+        borderLeft: hasDifferentAdjacent.left ? '1.5px solid black' : `none`,
+        borderRight: hasDifferentAdjacent.right ? '1.5px solid black' : `none`,
       }}
     ></div>
   );
